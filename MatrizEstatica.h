6897d7b7c4631b3f214c245af3b3e4112f3910fc
GeorginaRetaLimas <2430043@upv.edu.mx>
2025-10-20 17:37:10 -0600

    Matriz Estatica

--- MatrizEstatica.h
+++ MatrizEstatica.h
@@ -1,4 +1,21 @@
+#ifndef MATRIZESTATICA_H
+#define MATRIZESTATICA_H
+
+#include "MatrizBase.h"
+
 template <typename T, int M, int N>
 class MatrizEstatica : public MatrizBase<T> {
-    T _datos[M][N];
+    private:
+        T _datos[M][N];
+    
+    public:
+        // Constructor
+        MatrizEstatica();
+
+        // Métodos Virtuales
+        void cargarValores() override;
+        MatrizBase<T>* sumar(const MatrizBase<T>& otra) const override;
+        void imprimir() const override;
 };
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-// Conexión
+#include "MatrizBase.h"
+#include "MatrizDinamica.h"
+#include "MatrizEstatica.h"
+
+using namespace std;
 
 int main() {
+    std::cout<<"--- Sistema Genérico de Álgebra Lineal ---"<<std::endl<<std::endl;
+
     return 0;
 }
