2adcb5f2140896efd794cb612e3ca1b79addcc72
GeorginaRetaLimas <2430043@upv.edu.mx>
2025-10-20 17:05:46 -0600

    Matriz Base avance

--- MatrizBase.h
+++ MatrizBase.h
@@ -10,7 +10,26 @@ class MatrizBase {
         int _columnas;
     
     public:
-    virtual void cargarValores() = 0;
-    virtual MatrizBase<T>* sumar(const MatrizBase<T>& otra) const = 0;
-    virtual void imprimir() const = 0;
+        // Constructor
+        MatrizBase(int filas, int columnas) : 
+            _filas(filas), _columnas(columnas) {}
+
+        // Destructor
+        virtual ~MatrizBase() {}
+
+        // Métodos virtuales abstractos
+        virtual void cargarValores() = 0;
+        virtual MatrizBase<T>* sumar(const MatrizBase<T>& otra) const = 0;
+        virtual void imprimir() const = 0;
+
+        // Sobrecarga
+        MatrizBase<T>* operator+(const MatrizBase<T>& otra) const{
+            return this->sumar(otra);
+        }
+
+        // Getters
+        int getFilas() const{ return _filas}
+        int getColumnas() const{ return _columnas}
 };
+
+#endif
